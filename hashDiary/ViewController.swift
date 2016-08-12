739e46b6243ce1916400c21fea7837e4990c5f27
SHINTARO ASADA <9s0ner@gmail.com>
2016-08-12 15:07:08 +0800

    initcommit

---
+++ hashDiary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hashDiary
+//
+//  Created by 浅田真太郎 on 2016/08/12.
+//  Copyright © 2016年 浅田真太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
