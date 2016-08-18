8d6a485dccc39e3fe01a36d8310642205e1e2700
SHINTARO ASADA <9s0ner@gmail.com>
2016-08-18 15:33:12 +0800

    タイトルの保存

--- hashDiary/dayViewController.swift
+++ hashDiary/dayViewController.swift
@@ -13,16 +13,38 @@ class dayViewController: UIViewController, UITableViewDataSource,  UITableViewDe
 {
     @IBOutlet weak var celectedDateLabel: UILabel!
     @IBOutlet weak var myTableView: UITableView!
+    @IBOutlet weak var diaryTitle: UITextField!
     
     //今日の日付を表示
     var selectedDate = NSDate()
     
+    //日記のタイトル
+    var diaryList = [["title1":"タイトル1","date":"2016-05-13"],["title1":"タイトル2","date":"2016-05-14"],["title1":"タイトル3","date":"2016-05-15"]]
+    
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        
+        
+        //ユーザーデフォルトから保存した配列を取り出す
+        var myDefault = NSUserDefaults.standardUserDefaults()
+        //ユーザーデフォルトを全削除する
+       // var appDomain:String = NSBundle.mainBundle().bundleIdentifier!
+        //myDefault.removePersistentDomainForName(appDomain)
+
+        if (myDefault.objectForKey("diaryList") != nil)
+        {
+            //データ取り出し
+            diaryList = myDefault.objectForKey("diaryList") as! [Dictionary]
+        }
+        print(diaryList)
+        
+        
         // NavigationBarを隠す処理
         self.navigationController?.setNavigationBarHidden(true, animated: true)
-       
+        
         var df = NSDateFormatter()
         
         df.dateFormat = "yyyy/MM/dd"
@@ -83,6 +105,47 @@ class dayViewController: UIViewController, UITableViewDataSource,  UITableViewDe
             //cell.textLabel?.text = "文字列"
             return cell
     }
+    
+    @IBAction func saveBtn(sender: UIButton)
+    {
+      //タイトルの追加
+        diaryList.append(["title":diaryTitle.text!,"date":celectedDateLabel.text!])
+        
+        //ユーザーデフォルトに保存する作業
+        //ユーザーデフォルトを用意
+        var myDefault = NSUserDefaults.standardUserDefaults()
+        //データを書き込んで
+        myDefault.setObject(diaryList, forKey:"diaryList")
+        //即反映される
+        myDefault.synchronize()
+        
+        print("保存されました")
+        
+        
+        
+        
+        
+        
+    
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     override func didReceiveMemoryWarning()
     {
